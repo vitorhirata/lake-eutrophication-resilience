e7eef879ad4363499c7135499bee9d397793e789
Vitor Hirata Sanches <vitors.hirata@gmail.com>
2023-12-21 14:07:13 -0300

    Adds test for _entropy function
    The plan is to have a cover important functions with tests, to avoid
    unintented behaviour and faciliate changes in the code. It starts adding
    the test library and testing the _entropy function. This is the first
    function because of the need to refactor this function.

---
+++ test/common.jl
@@ -0,0 +1,70 @@
+@testset "#_entropy" begin
+    @testset "Low P0 and number of decisions one" begin
+        P0 = 0.5
+        I = 0.1
+        decision_step = 5.0
+        number_decision = 1
+        deterministic = true
+        maximum_options = 10
+
+        entropy = PathwayDiversity._entropy(P0, I, decision_step, number_decision, deterministic, maximum_options)
+        number_options = PathwayDiversity.number_possible_influx(P0, maximum_options)
+
+        @test number_options == 10
+        @test entropy ≈ 2.302585 rtol=1e-5
+    end
+
+    @testset "High P0 and number of decisions one" begin
+        P0 = 2.0
+        I = 0.1
+        decision_step = 5.0
+        number_decision = 1
+        deterministic = true
+        maximum_options = 10
+
+        entropy = PathwayDiversity._entropy(P0, I, decision_step, number_decision, deterministic, maximum_options)
+        number_options = PathwayDiversity.number_possible_influx(P0, maximum_options)
+
+        @test number_options == 4
+        @test entropy ≈ 1.386294 rtol=1e-5
+    end
+
+    @testset "Low P0 and number of decisions two" begin
+        P0 = 0.5
+        I = 0.1
+        decision_step = 5.0
+        number_decision = 2
+        deterministic = true
+        maximum_options = 10
+
+        entropy = PathwayDiversity._entropy(P0, I, decision_step, number_decision, deterministic, maximum_options)
+
+        @test entropy ≈ 4.605170 rtol=1e-5
+    end
+
+    @testset "High P0 and number of decisions two" begin
+        P0 = 2.0
+        I = 0.1
+        decision_step = 5.0
+        number_decision = 2
+        deterministic = true
+        maximum_options = 10
+
+        entropy = PathwayDiversity._entropy(P0, I, decision_step, number_decision, deterministic, maximum_options)
+
+        @test entropy ≈ 2.4849067 rtol=1e-5
+    end
+
+    @testset "High P0, number of decisions five and initial influx high" begin
+        P0 = 0.5
+        I = 0.15
+        decision_step = 5.0
+        number_decision = 6
+        deterministic = true
+        maximum_options = 10
+
+        entropy = PathwayDiversity._entropy(P0, I, decision_step, number_decision, deterministic, maximum_options)
+
+        @test entropy ≈ 12.129983 rtol=1e-5
+    end
+end
---
+++ test/runtests.jl
@@ -0,0 +1,5 @@
+using Test
+using PathwayDiversity
+
+include("./common.jl")
+

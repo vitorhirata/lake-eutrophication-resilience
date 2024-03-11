89d2166902d8a20ff257bd2118e245d2e3b99ec9
Vitor Hirata Sanches <vitors.hirata@gmail.com>
2024-03-11 17:25:05 +1100

    Extract computation of all tipping point and kendall
    To remove complexity of the early_warning_signals function it extracts
    the computation of all tipping point and kendal tau values to a function
    in Metrics.
    Possibly change this to use dictionaries.

--- src/metrics.jl
+++ src/metrics.jl
@@ -1,20 +1,19 @@
-function cross_threshold(time_series::NamedDimsArray, times::StepRangeLen{Float64},
-    base_influx::Float64, influx_tax::Float64, I_step::Float64 = 1.0
-)
-    influx_ts = [base_influx + influx_tax * (time ÷ I_step) for time in times]
-    thresholds = map(influx -> get_root(1.3, influx), influx_ts)
-    first_cross = findfirst(item -> item > 0, time_series - thresholds)
-    return first_cross
-end
+function threshold_points(p::NamedDimsArray, s::NamedDimsArray, times::StepRangeLen{Float64},
+        variance::NamedDimsArray, autocorrelation::NamedDimsArray,
+        base_influx::Float64, influx_tax::Float64, I_step::Float64 = 1.0
+)::Tuple{Dict, Dict}
+    points = Dict()
+    kendall_tau = Dict()
 
-function max_kendall_tau_idx(time_series::NamedDimsArray, times::StepRangeLen{Float64})::Tuple{Int64, Float64}
-    kendall_range = _kendall_range(time_series, times)
-    times = collect(times)
-    time_series = parent(time_series)
+    points[:p] = _cross_threshold(p, times, base_influx, influx_tax, I_step)
+    points[:var], kendall_tau[:var] = _max_kendall_tau(variance, times)
+    points[:autocorr], kendall_tau[:autocorr] = _max_kendall_tau(autocorrelation, times)
 
-    kendall = [corkendall(times[idx:end], time_series[idx:end]) for idx in kendall_range]
-    max_kendall = findmax(abs.(kendall))
-    return kendall_range[max_kendall[2]], max_kendall[1]
+    points[:s], kendall_tau[:s] = zeros(Int64, size(s, :time_horizon)), zeros(size(s, :time_horizon))
+    for idx in 1:size(s, :time_horizon)
+        points[:s][idx], kendall_tau[:s][idx] = _max_kendall_tau(s[time_horizon=idx], times)
+    end
+    return points, kendall_tau
 end
 
 function normalize_pd(time_series::NamedDimsArray)::NamedDimsArray
@@ -78,6 +77,25 @@ function loess_detrend(time_series::NamedDimsArray, times::StepRangeLen{Float64}
     smooth_function = predict(model, times)
     result = time_series - smooth_function
     return result
+end
+
+function _cross_threshold(time_series::NamedDimsArray, times::StepRangeLen{Float64},
+    base_influx::Float64, influx_tax::Float64, I_step::Float64 = 1.0
+)
+    influx_ts = [base_influx + influx_tax * (time ÷ I_step) for time in times]
+    thresholds = map(influx -> get_root(1.3, influx), influx_ts)
+    first_cross = findfirst(item -> item > 0, time_series - thresholds)
+    return first_cross
+end
+
+function _max_kendall_tau(time_series::NamedDimsArray, times::StepRangeLen{Float64})::Tuple{Int64, Float64}
+    kendall_range = _kendall_range(time_series, times)
+    times = collect(times)
+    time_series = parent(time_series)
+
+    kendall = [corkendall(times[idx:end], time_series[idx:end]) for idx in kendall_range]
+    max_kendall = findmax(abs.(kendall))
+    return kendall_range[max_kendall[2]], round(max_kendall[1]; digits=2)
 end
 
 function _kendall_range(time_series::NamedDimsArray, times::StepRangeLen{Float64})::StepRangeLen{Int64}

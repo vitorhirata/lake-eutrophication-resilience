b18760d5eab70b208bb4a66ec0b84967f1730be4
Vitor Hirata Sanches <vitors.hirata@gmail.com>
2024-06-10 02:46:35 +0200

    Fix number of colors in DT plot
    In the commit f235cd824390b15e195e97cd4033e32db8adf6db a new
    time_horizon was added. However, the number of colors in the DT was not
    changed accordingly. This commit fix this problem by adding one more
    color in the DT plot.

--- src/visualisation.jl
+++ src/visualisation.jl
@@ -61,9 +61,9 @@ function _plot_distance_threshold(s, s_diff, distance_threshold, time_horizons, 
     plt1 = plot(distance_threshold, s, label=label, legend=:left, xflip = true,
           ylabel="Pathway diversity", left_margin = 10Plots.mm)
     vline!([0.0], label=false, color="black")
-    plt2 = plot(distance_threshold[2:end], s_diff, label=label, legend=:left, color=[1 2 3], xflip = true,
+    plt2 = plot(distance_threshold[2:end], s_diff, label=label, legend=:left, color=[1 2 3 4], xflip = true,
           ylabel="Pathway diversity derivative", xlabel="Distance to threshold", left_margin = 10Plots.mm)
-    scatter!(distance_threshold[2:end][peaks_idx], peak_values, label=false, markerstrokewidth=0, color=[1, 2, 3])
+    scatter!(distance_threshold[2:end][peaks_idx], peak_values, label=false, markerstrokewidth=0, color=[1, 2, 3, 4])
     vline!([0.0], label=false, color="black")
 
     plot(plt1, plt2, layout=(2,1), size=(1000,1200), guidefontsize=12)
